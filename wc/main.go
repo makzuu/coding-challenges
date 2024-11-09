0f27c3628c8f156749ae54c7145a84b218600ac5
makzuu <mr.greenmousth@gmail.com>
2024-11-09 14:20:20 -0300

    count bytes

--- wc/main.go
+++ wc/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
+	"os"
 )
 
+func count_bytes(filename string) int {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Error reading file %v: %v", filename, err)
+	}
+	return len(data)
+}
+
 func main() {
-    fmt.Println("Hi!")
+	args := os.Args[1:]
+	if len(args) != 2 {
+		log.Fatalln("Error: 2 arguments expected: option, filename.")
+	}
+
+	option := args[0]
+	filename := args[1]
+
+	switch option {
+	case "-c":
+		result := count_bytes(filename)
+		fmt.Println(result, filename)
+	default:
+		log.Fatalf("Error: option %v not found", option)
+	}
 }
