3a66f7d70b36a029cd20c730420d823848413c14
makzuu <mr.greenmousth@gmail.com>
2025-05-18 14:34:34 -0400

    (cut): implement -d option

--- cut/cut.c
+++ cut/cut.c
@@ -10,8 +10,8 @@
 #define MAX_FILENAME_LEN 100
 
 bool todigit(char *c, int *result);
-void fields(int index, FILE *f);
-void extract(char *line, int index);
+void fields(int index, FILE *f, char delimiter);
+void extract(char *line, int index, char delimiter);
 
 struct option {
     char opt;
@@ -20,8 +20,9 @@ struct option {
 
 int main(int argc, char *argv[]) {
     list_t *options_list = list_init();
+    char delimiter = '\t';
 
-    char *optstring = ":f:";
+    char *optstring = ":f:d:";
     int opt;
     while ((opt = getopt(argc, argv, optstring)) != -1) {
 	switch (opt) {
@@ -34,6 +35,9 @@ int main(int argc, char *argv[]) {
 		opt->opt = 'f';
 		opt->optarg = optarg;
 		list_append(options_list, opt);
+		break;
+	    case 'd':
+		delimiter = *optarg;
 		break;
 	    case '?':
 		printf("cut: invalid option -- '%c'\n", optopt);
@@ -74,7 +78,7 @@ int main(int argc, char *argv[]) {
 			printf("cut: invalid field value '%c'\n", opt->opt);
 			exit(5);
 		    }
-		    fields(index, f);
+		    fields(index, f, delimiter);
 		    break;
 	    }
 	}
@@ -102,7 +106,7 @@ bool todigit(char *c, int *result) {
 #define MAX_LINE_LEN 500
 #define BUF_LEN 500
 
-void fields(int index, FILE *f) {
+void fields(int index, FILE *f, char delimiter) {
     char *line_buf = malloc(MAX_LINE_LEN);
     char c = 0;
     int i = 0;
@@ -110,7 +114,7 @@ void fields(int index, FILE *f) {
 	if (c == '\n') {
 	    line_buf[i] = '\0';
 	    i = 0;
-	    extract(line_buf, index);
+	    extract(line_buf, index, delimiter);
 	} else {
 	    if (i == MAX_LINE_LEN) {
 		printf("cut: max number of characters per line exceded\n");
@@ -127,20 +131,20 @@ void fields(int index, FILE *f) {
 
     if (i != 0) {
 	line_buf[i] = '\0';
-	extract(line_buf, index);
+	extract(line_buf, index, delimiter);
     }
 	
 
     free(line_buf);
 }
 
-void extract(char *line, int index) {
+void extract(char *line, int index, char delimiter) {
     size_t line_len = strlen(line);
     char *buf = malloc(line_len);
     int buf_idx = 0;
     int field_cnt = 1;
     for (size_t i = 0; i < line_len; i++) {
-	if (line[i] != '\t') {
+	if (line[i] != delimiter) {
 	    buf[buf_idx++] = line[i];
 	} else {
 	    buf[buf_idx] = '\0';
