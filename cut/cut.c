57be50f715442fb78c79f56b2cf994a06a637ccb
makzuu <mr.greenmousth@gmail.com>
2025-05-19 12:28:11 -0400

    (cut): organize main function

--- cut/cut.c
+++ cut/cut.c
@@ -10,8 +10,11 @@
 #define MAX_FILENAME_LEN 100
 
 bool todigit(char *c, int *result);
-void fields(int index, FILE *f, char delimiter);
-void extract(char *line, int index, char delimiter);
+void fields(int index, FILE *f, char *delimiter);
+void extract(char *line, int index, char *delimiter);
+void get_options(int argc, char *argv[], char *delimiter, list_t *options_list);
+void get_files(int argc, char *argv[], list_t *files_list);
+void process_options(char *delimiter, list_t *files_list, list_t *options_list);
 
 struct option {
     char opt;
@@ -22,69 +25,11 @@ int main(int argc, char *argv[]) {
     list_t *options_list = list_init();
     char delimiter = '\t';
 
-    char *optstring = ":f:d:";
-    int opt;
-    while ((opt = getopt(argc, argv, optstring)) != -1) {
-	switch (opt) {
-	    case 'f':
-		struct option *opt = malloc(sizeof(struct option));
-		if (opt == NULL) {
-		    fprintf(stderr, "could not allocate more memory\n");
-		    exit(1);
-		}
-		opt->opt = 'f';
-		opt->optarg = optarg;
-		list_append(options_list, opt);
-		break;
-	    case 'd':
-		delimiter = *optarg;
-		break;
-	    case '?':
-		printf("cut: invalid option -- '%c'\n", optopt);
-		exit(1);
-		break;
-	    case ':':
-		printf("cut: option requires an argument -- '%c'\n", optopt);
-		exit(2);
-		break;
-	}
-    }
-
     list_t *files_list = list_init();
-    for (; optind < argc; optind++) {
-	char *fname = malloc(MAX_FILENAME_LEN);
-	strncpy(fname, argv[optind], MAX_FILENAME_LEN);
-	fname[MAX_FILENAME_LEN - 1] = '\0';
-	list_append(files_list, fname);
-    }
 
-    for (int i = 0; i < files_list->len; i++) {
-	FILE *f = fopen(files_list->items[i], "r");
-	if (f == NULL) {
-	    if (errno == ENOENT) {
-		printf("cut: No such file -- '%s'\n", (char *)files_list->items[i]);
-		exit(4);
-	    }
-	    perror("fopen");
-	    exit(4);
-	}
-
-	for (int j = 0; j < options_list->len; j++) {
-	    struct option *opt = options_list->items[j];
-	    switch (opt->opt) {
-		case 'f':
-		    int index = 0;
-		    if (!todigit(opt->optarg, &index)) {
-			printf("cut: invalid field value '%c'\n", opt->opt);
-			exit(5);
-		    }
-		    fields(index, f, delimiter);
-		    break;
-	    }
-	}
-	
-	fclose(f);
-    }
+    get_options(argc, argv, &delimiter, options_list);
+    get_files(argc, argv, files_list);
+    process_options(&delimiter, files_list, options_list);
 
     list_destroy(files_list);
     list_destroy(options_list);
@@ -106,7 +51,7 @@ bool todigit(char *c, int *result) {
 #define MAX_LINE_LEN 500
 #define BUF_LEN 500
 
-void fields(int index, FILE *f, char delimiter) {
+void fields(int index, FILE *f, char *delimiter) {
     char *line_buf = malloc(MAX_LINE_LEN);
     char c = 0;
     int i = 0;
@@ -138,13 +83,13 @@ void fields(int index, FILE *f, char delimiter) {
     free(line_buf);
 }
 
-void extract(char *line, int index, char delimiter) {
+void extract(char *line, int index, char *delimiter) {
     size_t line_len = strlen(line);
     char *buf = malloc(line_len);
     int buf_idx = 0;
     int field_cnt = 1;
     for (size_t i = 0; i < line_len; i++) {
-	if (line[i] != delimiter) {
+	if (line[i] != *delimiter) {
 	    buf[buf_idx++] = line[i];
 	} else {
 	    buf[buf_idx] = '\0';
@@ -161,4 +106,74 @@ void extract(char *line, int index, char delimiter) {
     }
 
     free(buf);
+}
+
+
+void get_options(int argc, char *argv[], char *delimiter, list_t *options_list) {
+    char *optstring = ":f:d:";
+    int opt;
+    while ((opt = getopt(argc, argv, optstring)) != -1) {
+	switch (opt) {
+	    case 'f':
+		struct option *opt = malloc(sizeof(struct option));
+		if (opt == NULL) {
+		    fprintf(stderr, "could not allocate more memory\n");
+		    exit(1);
+		}
+		opt->opt = 'f';
+		opt->optarg = optarg;
+		list_append(options_list, opt);
+		break;
+	    case 'd':
+		*delimiter = *optarg;
+		break;
+	    case '?':
+		printf("cut: invalid option -- '%c'\n", optopt);
+		exit(1);
+		break;
+	    case ':':
+		printf("cut: option requires an argument -- '%c'\n", optopt);
+		exit(2);
+		break;
+	}
+    }
+}
+
+void get_files(int argc, char *argv[], list_t *files_list) {
+    for (; optind < argc; optind++) {
+	char *fname = malloc(MAX_FILENAME_LEN);
+	strncpy(fname, argv[optind], MAX_FILENAME_LEN);
+	fname[MAX_FILENAME_LEN - 1] = '\0';
+	list_append(files_list, fname);
+    }
+}
+
+void process_options(char *delimiter, list_t *files_list, list_t *options_list) {
+    for (int i = 0; i < files_list->len; i++) {
+	FILE *f = fopen(files_list->items[i], "r");
+	if (f == NULL) {
+	    if (errno == ENOENT) {
+		printf("cut: No such file -- '%s'\n", (char *)files_list->items[i]);
+		exit(4);
+	    }
+	    perror("fopen");
+	    exit(4);
+	}
+
+	for (int j = 0; j < options_list->len; j++) {
+	    struct option *opt = options_list->items[j];
+	    switch (opt->opt) {
+		case 'f':
+		    int index = 0;
+		    if (!todigit(opt->optarg, &index)) {
+			printf("cut: invalid field value '%c'\n", opt->opt);
+			exit(5);
+		    }
+		    fields(index, f, delimiter);
+		    break;
+	    }
+	}
+
+	fclose(f);
+    }
 }
